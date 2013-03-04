f27192c6ed5bd49fd4b0ef9a254ca87f2b4aa9ef
Jonathan Olson <olsonsjc@gmail.com>
2013-03-03 22:47:56 -0800

    More tests

--- tests.js
+++ tests.js
@@ -159,4 +159,257 @@ var diffs = [];
     }
   } );
   
+  addDiff( {
+    id: 'forwardAndBackQuadratic',
+    name: 'Forward and Back Quadratic',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.moveTo( -120, -120 );
+        context.quadraticCurveTo( 40, 40, 20, 20 );
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic1',
+    name: 'Cubic 1',
+    width: 44,
+    height: 44,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.translate( 20, 20 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( 2, 0, 2, 2, 4, 2 );
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'miterLimit',
+    name: 'Miter Limit',
+    width: 44,
+    height: 44,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        var halfAngle = 7 * Math.PI / 180;
+        context.moveTo( 10, 20 );
+        context.lineTo( 10 + Math.cos( halfAngle ), 20 + Math.sin( halfAngle ) );
+        context.lineTo( 10, 20 + 2 * Math.sin( halfAngle ) );
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubicCusp',
+    name: 'Cubic Cusp',
+    width: 84,
+    height: 72,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 20, 20 );
+        context.scale( 0.05, 0.05 );
+        context.moveTo( 102.6, 37.8 );
+        context.bezierCurveTo( 773.0, 708.2, 102.6, 708.2, 773.0,37.8 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic2',
+    name: 'Cubic 2',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( 1, 0, 0, 1, 1, 1 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic3',
+    name: 'Cubic 3',
+    width: 88,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( 40, 0, 41, 0, 41, 1 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic4',
+    name: 'Cubic 4',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        var smallNumber = 0.0000000001;
+        context.bezierCurveTo( smallNumber, smallNumber, smallNumber, smallNumber, smallNumber, smallNumber );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic5',
+    name: 'Cubic 5',
+    width: 64,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( 10, 0, 11, 0, 11, 1 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic5',
+    name: 'Cubic 5',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( 1, 0, 1, 1, 0, 0.5 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic6',
+    name: 'Cubic 6',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( -1, 0, 1, 0, 0, 0 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic7',
+    name: 'Cubic 7',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( -1, 0, 1, 0, 0, 0.1 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic8',
+    name: 'Cubic 8',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( -1, 0, 1, 0, 0.1, 0 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'cubic9',
+    name: 'Cubic 9',
+    width: 48,
+    height: 48,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.bezierCurveTo( 1, 1, 1, 1, 0, 0 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'lines1',
+    name: 'Lines 1',
+    width: 48,
+    height: 64,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.lineTo( 0, 10 );
+        context.lineTo( 10, 10 );
+        context.clip();
+        context.lineTo( 0, 15 );
+        context.restore();
+      } );
+    }
+  } );
+  
+  addDiff( {
+    id: 'miterLines',
+    name: 'Miter Lines',
+    width: 48,
+    height: 64,
+    draw: function( context ) {
+      demoPath( context, this.width, this.height, function() {
+        context.save();
+        context.miterLimit = 1000000;
+        context.translate( 24, 24 );
+        context.scale( 1, 1 );
+        context.moveTo( 0, 0 );
+        context.lineTo( 10, 0 );
+        context.lineTo( 0, 5 );
+        context.lineTo( Number.MAX_VALUE, 5 );
+        context.restore();
+      } );
+    }
+  } );
+  
 })();
